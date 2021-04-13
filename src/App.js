5a80ffa15e0393d202066388be5a747fc1bd6d49
hoang thinhh <hoangthinhh.vn44@gmail.com>
2021-04-13 11:14:41 +0700

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hoang thinh
+          thinh
         </p>
         <a
           className="App-link"
